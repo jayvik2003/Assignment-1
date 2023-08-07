bad121d8d4084967e0993558341f5fa0993b170c
jayvik2003 <ee22btech11025@iith.ac.in>
2023-08-07 15:20:50 +0530

    First Commit

--- codes/1.2.4.py
+++ codes/1.2.4.py
@@ -1,26 +1,47 @@
 import numpy as np
 
-from line_operations import line_section,dir_vec,line_intersect
+from line_operations import line_section,dir_vec,norm_vec
 
 A = np.array([1,-1])
-
 B = np.array([-4,6])
-
 C = np.array([-3,-5])
+D = np.array([0,0])
 E = np.array([0,0])
 F = np.array([0,0])
+G = np.array([-2,0])
 
-E = line_section(C,A,1)
+D = line_section(B,C,1.0)
+E = line_section(C,A,1.0)
 F = line_section(A,B,1.0)
 
-G=line_intersect(B,E,C,F)
+BG = dir_vec(B,G)
+GE = dir_vec(G,E)
+GF = dir_vec(G,F)
+CG = dir_vec(C,G)
+AG = dir_vec(A,G)
+GD = dir_vec(G,D)
 
-print("The intersection of BE and CF is ",G)
+n_BG = norm_vec(B,G)
+n_GE = norm_vec(G,E)
+n_GF = norm_vec(G,F)
+n_CG = norm_vec(C,G)
+n_AG = norm_vec(A,G)
+n_GD = norm_vec(G,D)
 
-#by manipulating the section formula for BGE we can find k 
+print("The direction vector of BG is",BG)
+print("The direction vector of GE is",GE)
+print("The direction vector of GF is",GF)
+print("The direction vector of CG is",CG)
+print("The direction vector of AG is",AG)
+print("The direction vector of GD is",GD)
 
-k = (G[0]-B[0])/(E[0]-G[0])
+print("The norm of the vector BG is",n_BG)
+print("The norm of the vector GE is",n_GE)
+print("The norm of the vector GF is",n_GF)
+print("The norm of the vector CG is",n_CG)
+print("The norm of the vector AG is",n_AG)
+print("The norm of the vector GD is",n_GD)
 
-print("the value of k in the section formula for BGE is",k)
-
-
+print("The ratio BG/GE is",n_BG/n_GE)
+print("The ratio CG/GF is",n_CG/n_GF)
+print("The ratio AG/GD is",n_AG/n_GD)
--- codes/line_operations.py
+++ codes/line_operations.py
@@ -1,8 +1,16 @@
 import numpy as np
 
+#for finding the direction vector
 def dir_vec(A, B):
     return B - A
 
+#for finding the norm of the vector
+def norm_vec(A,B):
+    N = np.array([0.0,0.0])
+    N = np.linalg.norm(B-A)
+    return N
+
+#section formula
 def line_section(A,B,k):
     P = np.array([0.0,0.0])
     P[0] = ((B[0]*k)+A[0])/(k+1)
@@ -10,13 +18,3 @@ def line_section(A,B,k):
 
     return P
 
-def line_intersect(B,E,C,F):
-    x=np.array([0,0])
-    m1=dir_vec(B,E)
-    m2=dir_vec(C,F)
-    #using the 1.1.4 equations
-    x[0]=(m2[0]*B[0]-m1[0]*C[0])/(m2[0]-m1[0])
-    x[1]=(m2[1]*B[1]-m1[1]*C[1])/(m2[1]-m1[1])
-
-
-    return x
